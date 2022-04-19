4ad72f77f58ccf1940b1550bac1fc68671833007
Joseph <71464700+jhangstein@users.noreply.github.com>
2022-04-19 13:07:12 +0200

    Visual implementation of timer with dummy button

--- app/src/main/java/com/example/melanzano/screens/ClockScreen.kt
+++ app/src/main/java/com/example/melanzano/screens/ClockScreen.kt
@@ -1,36 +1,97 @@
 package com.example.melanzano.screens
 
+import android.support.v4.os.IResultReceiver
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme.typography
+import androidx.compose.material.icons.Icons
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.melanzano.viewmodels.NoteViewModel
+import com.example.melanzano.viewmodels.TimerViewModel
 import com.example.melanzano.widgets.AddNoteWidget
 import com.example.melanzano.widgets.NoteCards
+import java.text.DecimalFormat
+import java.text.NumberFormat
 
 @Preview(showBackground = true)
 @Composable
-fun ClockScreen(viewModel: NoteViewModel = viewModel()){
+fun ClockScreen(viewModel: TimerViewModel = viewModel()) {
 
-        Column(modifier = Modifier
+    val f: NumberFormat = DecimalFormat("00")
+
+    var timerStarted by remember {
+        mutableStateOf(false)
+    }
+    
+    Column(
+        modifier = Modifier
             .fillMaxWidth()
             .fillMaxHeight()
             .padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-            Text(text = "HALLO HIER KOMMT NE UHR HER")
-            Text(text = "")
-            Text(text = "und vielleicht auch eine Melanzani")
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 2.dp, end = 2.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
 
+            Text(
+                text = "00",
+                fontSize = 40.sp,
+                color = Color.Black,
+                style = typography.caption
+            )
+
+            Text(text = ":",
+                fontSize = 30.sp,
+                color = Color.Black,
+                style = typography.caption,
+            )
+
+            Text(
+                text = f.format((viewModel.timeInSeconds.value / 60).toInt()),
+                fontSize = 40.sp,
+                color = Color.Black,
+                style = typography.caption
+            )
+
+            Text(text = ":",
+                fontSize = 30.sp,
+                color = Color.Black,
+                style = typography.caption)
+            
+            Text(
+                text = f.format((viewModel.timeInSeconds.value % 60)),
+                fontSize = 40.sp,
+                color = Color.Black,
+                style = typography.caption
+            )
         }
+        Button(onClick = {
+            timerStarted = !timerStarted
+            viewModel.startTimeCounter()
 
-
+        }) {
+            if (timerStarted){
+                Text(text = "Stop the count!")
+            }
+            else {
+                Text(text = "Start timer")
+            }
+        }
+    }
 
 
 }
