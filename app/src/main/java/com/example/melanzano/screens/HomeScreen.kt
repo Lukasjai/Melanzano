217b0c63080f5517d723c05052f7c5ee578233a6
Lukas <lukas.jaiczay@fh-campuswien.ac.at>
2022-04-22 23:04:13 +0200

    added coil and coil-compose and through this our melanzano picture

--- app/src/main/java/com/example/melanzano/screens/HomeScreen.kt
+++ app/src/main/java/com/example/melanzano/screens/HomeScreen.kt
@@ -1,31 +1,49 @@
 package com.example.melanzano.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Face
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.melanzano.models.Note
+import coil.compose.AsyncImage
 import com.example.melanzano.viewmodels.NoteViewModel
 import com.example.melanzano.widgets.NoteCards
 
 @Composable
-fun HomeScreen(viewModel: NoteViewModel = viewModel()){
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .fillMaxHeight(),
+fun HomeScreen(viewModel: NoteViewModel = viewModel()) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Melanzano",
-            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
-            style = MaterialTheme.typography.h5)
+        Row(
+            modifier = Modifier.padding(12.dp)
+        ) {
+            Text(
+                text = "Melanzano",
+                modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
+                style = MaterialTheme.typography.h5
+            )
+
+            //adds our favourite vegetable and trademarks sign :)
+            AsyncImage(
+                model = "https://cdn.pixabay.com/photo/2020/03/28/17/01/eggplant-4977808_960_720.png",
+                contentDescription = null,
+                modifier = Modifier.size(50.dp)
+            )
+        }
+
         Text(text = "Your To Do´s:")
 
         NoteCards(
             notes = viewModel.getAllNotes()
-        ){ note ->
+        ) { note ->
             viewModel.removeNote(note)
         }
     }
