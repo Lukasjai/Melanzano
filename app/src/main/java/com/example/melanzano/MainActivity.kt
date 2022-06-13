b59163b8e7381417d0b8504b5886ffc9fbc14517
jhangstein <joseph.hangstein@stud.fhcampus-wien.ac.at>
2022-06-13 20:19:47 +0200

    Changed weird plus icon to weird play icon

--- app/src/main/java/com/example/melanzano/MainActivity.kt
+++ app/src/main/java/com/example/melanzano/MainActivity.kt
@@ -10,10 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.filled.Done
-import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -90,7 +87,7 @@ class MainActivity : ComponentActivity() {
                                 },
                             )
                             BottomNavigationItem(
-                                icon = { Icon(imageVector = Icons.Default.AddCircle, contentDescription = "clockscreen") },
+                                icon = { Icon(imageVector = Icons.Filled.PlayArrow, contentDescription = "clockscreen") },
                                 selected = currentRoute == "clockscreen",
                                 onClick = {
                                     navController.navigate("clockscreen")
